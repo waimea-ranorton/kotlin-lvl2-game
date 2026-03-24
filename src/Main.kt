59e48d9c132f8dad29fe9ca71c16dd22dff9fe5b
waimea-ranorton <ranorton@waimea.school.nz>
2026-03-24 15:09:57 +1300

    Rearranging code and working towards fixing errors

--- src/Main.kt
+++ src/Main.kt
@@ -13,45 +13,33 @@ import java.security.Principal
  * =====================================================================
  */
 
-
 val board = mutableListOf<String>()
 fun main() {
-    //header
+//header----------------------------------------------------------------------------------------------------------------
     println("=========================".cyan())
     println("\uD83D\uDCA3    CHAIN REACTION   \uD83D\uDCA3".blue())
     println("=========================".cyan())
     println("")
 
-
 //getting player names--------------------------------------------------------------------------------------------------
-    var player1: String?
-    while (true) {
-        println("Hello Players")
-        println("Insert Player 1 name ")
-        player1 = readlnOrNull()
-        if (player1 != null && player1.isNotBlank()) break
-    }
 
-    var player2: String?
-    while (true) {
-        println("Hello Players")
-        println("Insert Player 2 name ")
-        player2 = readlnOrNull()
-        if (player2 != null && player2.isNotBlank()) break
-    }
+    getNames()
 
-//----------------------------------------------------------------------------------------------------------------------
-
-
-//putting the gameboard up
+//putting the gameboard up----------------------------------------------------------------------------------------------
     createboard()
-    while(true) {
+    while (true) {
         showboard()
         break
     }
+
+//Players place bombs---------------------------------------------------------------------------------------------------
+    repeat(times = 12) {
+        placebomb()
+    }
+
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+
 //board
 fun createboard() {
     repeat(12) {
@@ -78,14 +66,40 @@ fun showboard() {
 
 }
 
-//----------------------------------------------------------------------------------------------------------------------
 
-//getting player move x2
-fun placebomb(): Int {
+//Get names funcation--------------------------------------------------------------------------------------------------------------------
+
+
+fun getNames() {
+    var player1: String?
     while (true) {
-        print("Player 1 Enter sqaure")
+        println("Hello Players")
+        println("Insert Player 1 name: ")
+        player1 = readlnOrNull()
+        if (player1 != null && player1.isNotBlank()) break
+    }
+
+    var player2: String?
+    while (true) {
+        println("Insert Player 2 name: ")
+        player2 = readlnOrNull()
+        if (player2 != null && player2.isNotBlank()) break
     }
 }
+
+//Placebomb fnction-----------------------------------------------------------------------------------------------------------------
+fun placebomb() {
+    var location: Int?
+    while (true) {
+        println(" $player1 Enter square: ")
+        println("Options")
+        println("1,2,3,4,5,6,7,8,9,10,11,12")
+        location = readlnOrNull()?.toIntOrNull()
+        if (location != null && location in 1..12) break
+    }
+}
+
+
 //
 //fun updateboard(): {
 //
